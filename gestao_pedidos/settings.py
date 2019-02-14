df1f207210b9958a97efcd64cc52a1f75bf87687
Kenio Natan <kenionatan@gmail.com>
2019-02-14 09:59:43 -0200

    Allowing hosts

--- gestao_pedidos/settings.py
+++ gestao_pedidos/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
