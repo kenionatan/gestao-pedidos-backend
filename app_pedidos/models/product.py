ea8a1a6716ad61fe1268664834d5dff1e0f628a1
Kenio Natan <kenionatan@gmail.com>
2019-02-15 08:52:12 -0200

    Fixing max_digits attribute in product model

--- app_pedidos/models/product.py
+++ app_pedidos/models/product.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     product_title = models.CharField(max_length=100)
-    product_price = models.DecimalField(max_digits=8, decimal_places=2)
+    product_price = models.DecimalField(max_digits=12, decimal_places=2)
     product_multiple = models.PositiveIntegerField(blank=True, null=True)
 
     def __str__(self):
